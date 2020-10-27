a6b338ab1d68a35b76e9d1f051740b98968c899a
Rimantas <belauzas@yahoo.com>
2020-10-27 12:57:01 +0200

    ES6

--- js/main.js
+++ js/main.js
@@ -1,33 +1,77 @@
-let nameDOM = document.querySelector('#name');
-let lastnameDOM = document.querySelector('#lastname');
-let emailDOM = document.querySelector('#email');
-let passwordDOM = document.querySelector('#pass');
-let buttonDOM = document.querySelector('button');
+"use strict";
+(function () {
 
-let name = localStorage.getItem('name');
-let lastname = localStorage.getItem('lastname');
-let email = localStorage.getItem('email');
-let password = localStorage.getItem('password');
+    var nameDOM = document.querySelector('#name');
+    var lastnameDOM = document.querySelector('#lastname');
+    var emailDOM = document.querySelector('#email');
+    var passwordDOM = document.querySelector('#pass');
+    var buttonDOM = document.querySelector('button');
 
-nameDOM.value = name;
-lastnameDOM.value = lastname;
-emailDOM.value = email;
-passwordDOM.value = password;
+    var name = localStorage.getItem('name');
+    var lastname = localStorage.getItem('lastname');
+    var email = localStorage.getItem('email');
+    var password = localStorage.getItem('password');
 
-buttonDOM.addEventListener('click', (e) => {
-    console.log(e.preventDefault());
+    nameDOM.value = name;
+    lastnameDOM.value = lastname;
+    emailDOM.value = email;
+    passwordDOM.value = password;
 
-    if (nameDOM.value.indexOf('<') >= 0 ||
-        nameDOM.value.indexOf('>') >= 0 ||
-        nameDOM.value.indexOf('/') >= 0) {
-        console.log('ERROR');
-        return;
+    buttonDOM.addEventListener('click', (e) => {
+        console.log(e.preventDefault());
+
+        if (nameDOM.value.indexOf('<') >= 0 ||
+            nameDOM.value.indexOf('>') >= 0 ||
+            nameDOM.value.indexOf('/') >= 0) {
+            console.log('ERROR');
+            return;
+        }
+
+        localStorage.setItem('name', nameDOM.value);
+        localStorage.setItem('lastname', lastnameDOM.value);
+        localStorage.setItem('email', emailDOM.value);
+        localStorage.setItem('password', passwordDOM.value);
+
+        location.href = "./confirm";
+    })
+
+
+    var transfer = (amount) => {
+        var magic = 'abra kadabra';
+        return amount;
     }
 
-    localStorage.setItem('name', nameDOM.value);
-    localStorage.setItem('lastname', lastnameDOM.value);
-    localStorage.setItem('email', emailDOM.value);
-    localStorage.setItem('password', passwordDOM.value);
+})();
 
-    location.href = "./confirm";
-})
+
+
+
+(function () {
+    console.log('triple -=xXx=-');
+})()
+
+
+
+const pazymiai = [10, 8, 4, 8, 7, 5];
+
+console.log(pazymiai.map(pazimys => Math.ceil(pazimys / 2)));
+
+
+const zodziai = ['labas', 'rytas', 'ka', 'tu', 'veiki'];
+
+const pirmosRaides = zodziai.map(zodis => zodis[0])
+
+console.log(pirmosRaides);
+
+console.log(pazymiai.map(x => x * x));
+
+console.log(zodziai.filter(z => z.includes('a')));
+
+console.log(pazymiai.filter(p => p >= 6 && p < 9));
+
+const vidurkis = pazymiai.reduce((total, num) => total + num, 0) / pazymiai.length;
+
+console.log(vidurkis);
+
+console.log(pazymiai.sort((a, b) => b - a));
+console.log(zodziai.sort());
